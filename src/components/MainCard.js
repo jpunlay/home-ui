3cd467170282c440716f26cacbd2ed2dc91ddf40
Jesus Punlay <jpunlay@gmail.com>
2020-05-17 22:57:12 -0400

    update opened card handle

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
-import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
-import styled from "styled-components";
-import { MDBView, MDBMask } from "mdbreact";
 import "holderjs";
 import About from "./About";
 import Hobbies from "./Hobbies";
@@ -10,48 +7,52 @@ import Projects from "./Projects";
 import Articles from "./Articles";
 import Life from "./Life";
 import ProjectsDescription from "./ProjectsDescription";
-import ph from "./../images/background.jpg";
-
-const StyledCard = styled(Card)`
-  background-color: #000000 !important;
-  height: 100% !important;
-  border: 2px solid white; // temp for visualisation
-`;
 
 class MainCard extends React.Component {
   constructor(props) {
     super(props);
     this.handleOpenClick = this.handleOpenClick.bind(this);
     this.state = { isOpen: false };
+    this.state = { openedCard: null };
   }
 
-  handleOpenClick() {
+  handleOpenClick(openedCard) {
     this.setState({ isOpen: true });
-    console.log(this.isOpen)
+    switch (openedCard) {
+      case "project":
+        this.setState({ openedCard: <ProjectsDescription></ProjectsDescription> });
+        break;
+      case "hobbies":
+        // code block
+        break;
+      default:
+      // code block
+    }
   }
 
   render() {
     const isOpen = this.state.isOpen;
+    const openedCard = this.state.openedCard;
     let main;
 
     if (!isOpen) {
       main =
-        <div className="mainCards">
+        <div className="mainCard">
           <CardGroup>
-            <Projects onClick={this.handleOpenClick}></Projects>
-            <Hobbies onClick={this.handleOpenClick}></Hobbies>
+            <Projects onClick={() => this.handleOpenClick('project')}></Projects>
+            <Hobbies onClick={this.handleOpenClick('hobbies')}></Hobbies>
           </CardGroup>
           <CardGroup>
-            <About onClick={this.handleOpenClick}></About>
-            <Articles onClick={this.handleOpenClick}></Articles>
-            <Life onClick={this.handleOpenClick}></Life>
+            <About onClick={this.handleOpenClick('about')}></About>
+            <Articles onClick={this.handleOpenClick('articles')}></Articles>
+            <Life onClick={this.handleOpenClick('life')}></Life>
           </CardGroup>
         </div>;
     } else {
       main =
-        <StyledCard bg="dark" text="light">
-          <ProjectsDescription></ProjectsDescription>
-        </StyledCard>;
+        <div className="descriptionCard">
+          {openedCard}
+        </div>;
     }
 
     return (
@@ -60,13 +61,5 @@ class MainCard extends React.Component {
       </div>
     );
   }
-
-  // } else {
-  //   return (
-  //     <StyledCard bg="dark" text="light" onClick={() => setProjectView(false)}>
-  //       <ProjectsDescription></ProjectsDescription>
-  //     </StyledCard>
-  //   );
-  // }
 }
 export default MainCard;
--- src/components/ProjectsDescription.js
+++ src/components/ProjectsDescription.js
@@ -4,6 +4,12 @@ import { MDBView, MDBMask } from "mdbreact";
 import sign from "./../images/sign.jpg";
 import Card from "react-bootstrap/Card";
 
+const StyledCard = styled(Card)`
+    background-color: #000000!important;
+    height: 100%!important;
+    border: 2px solid white; // temp for visualisation
+`;
+
 const StyledText = styled.div`
   margin: 0;
   position: absolute;
@@ -15,40 +21,43 @@ const StyledText = styled.div`
 
 function ProjectsDescription(props) {
   return (
-    <MDBView>
-      <Card.Img variant="top" src={sign} />
-      <MDBMask>
-        <StyledText>
-          <p className="grey-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+    <StyledCard bg="dark" text="light" onClick={props.onClick}>
+
+      <MDBView>
+        <Card.Img variant="top" src={sign} />
+        <MDBMask>
+          <StyledText>
+            <p className="grey-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <p className="grey-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            <p className="grey-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          <p className="grey-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            <p className="grey-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+              ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+              aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+              pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum.
           </p>
-        </StyledText>
-      </MDBMask>
-    </MDBView>
+          </StyledText>
+        </MDBMask>
+      </MDBView>
+    </StyledCard>
   );
 }
 

95d7eadf8808943ba2337294da63fa4635030587
Jesus Punlay <jpunlay@gmail.com>
2020-05-17 22:00:46 -0400

    Update coditional statement, use component

--- src/components/MainCard.js
+++ src/components/MainCard.js
@@ -18,35 +18,55 @@ const StyledCard = styled(Card)`
   border: 2px solid white; // temp for visualisation
 `;
 
-function MainCard(props) {
-  const [isProjectOpen, setProjectView] = useState(false);
+class MainCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleOpenClick = this.handleOpenClick.bind(this);
+    this.state = { isOpen: false };
+  }
 
-  if (!isProjectOpen) {
+  handleOpenClick() {
+    this.setState({ isOpen: true });
+    console.log(this.isOpen)
+  }
+
+  render() {
+    const isOpen = this.state.isOpen;
+    let main;
+
+    if (!isOpen) {
+      main =
+        <div className="mainCards">
+          <CardGroup>
+            <Projects onClick={this.handleOpenClick}></Projects>
+            <Hobbies onClick={this.handleOpenClick}></Hobbies>
+          </CardGroup>
+          <CardGroup>
+            <About onClick={this.handleOpenClick}></About>
+            <Articles onClick={this.handleOpenClick}></Articles>
+            <Life onClick={this.handleOpenClick}></Life>
+          </CardGroup>
+        </div>;
+    } else {
+      main =
+        <StyledCard bg="dark" text="light">
+          <ProjectsDescription></ProjectsDescription>
+        </StyledCard>;
+    }
+
     return (
-      <div className="mainCards">
-        <CardGroup>
-          <StyledCard
-            bg="dark"
-            text="light"
-            onClick={() => setProjectView(true)}
-          >
-            <Projects></Projects>
-          </StyledCard>
-          <Hobbies></Hobbies>
-        </CardGroup>
-        <CardGroup>
-          <About></About>
-          <Articles></Articles>
-          <Life></Life>
-        </CardGroup>
+      <div>
+        {main}
       </div>
     );
-  } else {
-    return (
-      <StyledCard bg="dark" text="light" onClick={() => setProjectView(false)}>
-        <ProjectsDescription></ProjectsDescription>
-      </StyledCard>
-    );
   }
+
+  // } else {
+  //   return (
+  //     <StyledCard bg="dark" text="light" onClick={() => setProjectView(false)}>
+  //       <ProjectsDescription></ProjectsDescription>
+  //     </StyledCard>
+  //   );
+  // }
 }
 export default MainCard;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,14 +4,22 @@ import { MDBView, MDBMask } from "mdbreact";
 import ph from "./../images/background.jpg";
 import Card from "react-bootstrap/Card";
 
-function Projects() {
+const StyledCard = styled(Card)`
+    background-color: #000000!important;
+    height: 100%!important;
+    border: 2px solid white; // temp for visualisation
+`;
+
+function Projects(props) {
   return (
-    <MDBView hover zoom>
-      <Card.Img variant="top" src={ph} />
-      <MDBMask className="flex-center">
-        <p className="grey-text">{"> Hello World"}</p>
-      </MDBMask>
-    </MDBView>
+    <StyledCard bg="dark" text="light" onClick={props.onClick}>
+      <MDBView hover zoom>
+        <Card.Img variant="top" src={ph} />
+        <MDBMask className="flex-center">
+          <p className="grey-text">{"> Hello World"}</p>
+        </MDBMask>
+      </MDBView>
+    </StyledCard>
   );
 }
 

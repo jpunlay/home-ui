5a7ba76ee3f64add9d14e338a1b7cdaae0847f99
Jesus Punlay <jpunlay@gmail.com>
2020-05-23 16:24:16 -0400

    handle open and close from topnav to modal

---
+++ src/components/Resume.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+class Resume extends React.Component {
+    render() {
+        return (
+            <>
+                <Modal size="xl" show={this.props.showResume} onHide={this.props.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Jesus Punlay Resume <br></br>
+                            <pre>(123)456-7890  |  jpunlay@gmail.com  |  101 main stareet; Knightdale, NC; 27545</pre>
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <p>
+                            Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
+                            commodi aspernatur enim, consectetur. Cumque deleniti temporibus
+                            ipsam atque a dolores quisquam quisquam adipisci possimus
+                            laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
+                            accusamus eos quod.
+                        </p>
+                        <h2>Education</h2>
+                        <p>
+                            Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
+                            commodi aspernatur enim, consectetur. Cumque deleniti temporibus
+                            ipsam atque a dolores quisquam quisquam adipisci possimus
+                            laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
+                            accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
+                            reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
+                            deleniti rem!
+                        </p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="light" onClick={this.props.handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="dark" onClick={this.props.handleClose}>
+                            Download
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+
+            </>
+        )
+    }
+}
+
+export default Resume
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -3,8 +3,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import styled from 'styled-components';
 import FollowAt from "react-social-media-follow";
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
+import Resume from "./Resume";
 
 const StyledNavBar = styled(Navbar)`
     background-color: #000000!important;
@@ -28,17 +27,13 @@ class TopNav extends React.Component {
     }
 
     handleShow() {
-        console.log("show");
         this.setState({ showResume: true });
     }
     handleClose() {
-        console.log("close");
         this.setState({ showResume: false });
     }
 
     render() {
-        const showResume = this.state.showResume;
-
         return (
             <div className="topNav">
                 <StyledNavBar bg="dark" variant="dark" expand="lg">
@@ -47,53 +42,19 @@ class TopNav extends React.Component {
                     </StyledNavBarBrand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ml-auto" pullRight>
+                        <Nav className="ml-auto">
                             <Nav.Link href="/dope">blog</Nav.Link>
-                            <Nav.Link href="#resume" onClick={this.handleShow}>resume</Nav.Link>
+                            <Nav.Link href="#resume" onClick={() => this.handleShow()}>resume</Nav.Link>
                             <Nav.Link href="#feedback">feedback</Nav.Link>
-                            <Nav.Link href="#repo"><FollowAt links={repository} color="grey" hoverColor="white" iconSize="1" /></Nav.Link>
+                            <div style={{padding: '.5rem'}}>
+                                <FollowAt links={repository} color="grey" hoverColor="white" iconSize="1"/>
+                            </div>
                         </Nav>
                     </Navbar.Collapse>
                 </StyledNavBar>
 
                 {/* Resume popup modal */}
-                <Modal size="xl" show={showResume} onHide={this.handleClose}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Jesus Punlay Resume <br></br>
-                            <pre>(123)456-7890     |     jpunlay@gmail.com     |     101 main stareet; Knightdale, NC; 27545</pre>
-                        </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <p>
-                            Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
-                            commodi aspernatur enim, consectetur. Cumque deleniti temporibus
-                            ipsam atque a dolores quisquam quisquam adipisci possimus
-                            laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
-                            accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
-                            reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
-                            deleniti rem!
-                        </p>
-                        <h2>Education</h2>
-                        <p>
-                            Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
-                            commodi aspernatur enim, consectetur. Cumque deleniti temporibus
-                            ipsam atque a dolores quisquam quisquam adipisci possimus
-                            laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
-                            accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
-                            reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
-                            deleniti rem!
-                        </p>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="light" onClick={this.handleClose}>
-                            Close
-                        </Button>
-                        <Button variant="dark" onClick={this.handleClose}>
-                            Save Changes
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
-
+                <Resume showResume={this.state.showResume} handleClose={this.handleClose}></Resume>
             </div>
         );
     }

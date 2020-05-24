1cc9ff4c3bb02244c3292a1b6278de132f81e822
Jesus Punlay <jpunlay@gmail.com>
2020-05-24 13:13:33 -0400

    Add resume placeholders

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -20,6 +20,26 @@ class Resume extends React.Component {
                             laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
                             accusamus eos quod.
                         </p>
+                        <h2>Experience</h2>
+                        <p>
+                            Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
+                            commodi aspernatur enim, consectetur. Cumque deleniti temporibus
+                            ipsam atque a dolores quisquam quisquam adipisci possimus
+                            laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
+                            accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
+                            reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
+                            deleniti rem!
+                        </p>
+                        <h2>Languages | Frameworks | Other skills</h2>
+                        <p>
+                            Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
+                            commodi aspernatur enim, consectetur. Cumque deleniti temporibus
+                            ipsam atque a dolores quisquam quisquam adipisci possimus
+                            laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
+                            accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
+                            reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
+                            deleniti rem!
+                        </p>
                         <h2>Education</h2>
                         <p>
                             Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
